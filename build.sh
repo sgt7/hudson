a6a613d91ab0c8dbfc863c0ca57e2bb91d0056cd
Chirayu Desai <chirayudesai1@gmail.com>
2012-06-01 14:46:39 +0530

    derp

--- build.sh
+++ build.sh
@@ -86,7 +86,7 @@ then
   CORE_BRANCH=$REPO_BRANCH
 fi
 rm -rf .repo/manifests*
-repo init -u $SYNC_PROTO://github.com/CyanogenMod/android.git -b $CORE_BRANCH
+repo init -u $SYNC_PROTO://github.com/sgt7/android.git -b $CORE_BRANCH
 check_error "repo init failed."
 
 # make sure ccache is in PATH
