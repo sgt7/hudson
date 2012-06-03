49ac65c1e954b04fd3ff125f20e7637a2d9d5132
Chirayu Desai <chirayudesai1@gmail.com>
2012-06-03 18:07:54 +0530

    stuff

--- build.sh
+++ build.sh
@@ -119,9 +119,6 @@ fi
 lunch $LUNCH
 check_result "lunch failed."
 
-# save manifest used for build (saving revisions as current HEAD)
-repo manifest -o $WORKSPACE/archive/manifest.xml -r
-
 rm -f $OUT/cm-*.zip*
 
 UNAME=$(uname)
